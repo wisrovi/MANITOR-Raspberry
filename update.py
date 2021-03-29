4c0581dc331a7bfb532eb4783d4581960598dad2
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-03-29 16:29:12 -0500

    update

--- update.py
+++ update.py
@@ -2,9 +2,10 @@
 import os.path as path
 
 from Process.AutoUpdate.autoupdate import Autoupdate
+from decouple import config
 
 
-@Autoupdate(name="Autoupdate WISROVI", project="wisrovi/MANITOR-Raspberry", root_path=path.dirname(path.realpath(__file__)))
+@Autoupdate(name="Autoupdate WISROVI", project=config('project', default=''), root_path=path.dirname(path.realpath(__file__)))
 def main_demo_autoupdate():
     print("update library")
 
