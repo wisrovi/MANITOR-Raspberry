c8a12c069b2d90da4bb9c169279e7687fcfdd489
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-04-13 18:55:20 -0500

    Update test_sound.py

--- resources/test_sound.py
+++ resources/test_sound.py
@@ -1,19 +1,11 @@
+from multiprocessing import Process
+
 from playsound import playsound
 
-# from library_reproduce_audio.SoundUtil import Sonido
-from concurrent.futures import ThreadPoolExecutor
+def audio():
+    playsound("p0.mp3")
 
 
-def reproducir_audio(path_audio="p0.mp3"):
-    playsound(path_audio)
 
 
-if __name__ == "__main__":
-    path_audio = "p0.mp3"
-
-
-    #currs = ThreadPoolExecutor(max_workers=5)
-    #currs.submit(reproducir_audio, path_audio)
-
-    reproducir_audio(path_audio)
-
+Process(target=audio).start()
