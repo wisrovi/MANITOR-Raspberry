b3027502cc32df939c39c3bd22a83848a1477ef3
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-04-13 18:45:03 -0500

    update

--- Process/Sound/SoundUtil.py
+++ Process/Sound/SoundUtil.py
@@ -1,10 +1,9 @@
-from Process.Sound.Instrucciones_Guiones import GUIONES, PATH_VIDEOS
+from Instrucciones_Guiones import GUIONES, PATH_VIDEOS
 
 
 class Sonido(object):
     from playsound import playsound
     from concurrent.futures import ThreadPoolExecutor
-
 
     dialogos = list()
 
@@ -12,27 +11,23 @@ class Sonido(object):
         if path is None:
             path = PATH_VIDEOS
         self.dialogos = [path + "p" + str(id_dialogo) + ".wav" for id_dialogo in [diag['id'] for diag in GUIONES]]
-        print(self.dialogos)
+        # print(self.dialogos)
 
     def __reproducir_audio(self, id_audio):
         if id_audio > len(self.dialogos):
             id_audio = len(self.dialogos)
         audio = self.dialogos[id_audio]
-        print(audio)
-        self.playsound(audio)
 
+        audio = "../p0.mp3"
+        self.playsound(audio)
+        print(audio)
 
     def reproducir(self, id_audio):
-        currs = self.ThreadPoolExecutor(max_workers=5)
-        currs.submit(self.__reproducir_audio, id_audio)
+        self.__reproducir_audio(0)
+        #currs = self.ThreadPoolExecutor(max_workers=5)
+        #currs.submit(self.__reproducir_audio, id_audio)
 
 
 if __name__ == "__main__":
     s = Sonido("../../resources/")
     s.reproducir(0)
-
-    import time
-
-    time.sleep(5)
-
-    s.reproducir(2)
--- resources/test_sound.py
+++ resources/test_sound.py
@@ -1,3 +1,19 @@
 from playsound import playsound
 
-playsound("p0.mp3")
+# from library_reproduce_audio.SoundUtil import Sonido
+from concurrent.futures import ThreadPoolExecutor
+
+
+def reproducir_audio(path_audio="p0.mp3"):
+    playsound(path_audio)
+
+
+if __name__ == "__main__":
+    path_audio = "p0.mp3"
+
+
+    currs = ThreadPoolExecutor(max_workers=5)
+    currs.submit(reproducir_audio, path_audio)
+
+    #playsound(path_audio)
+
