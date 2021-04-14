5940dcc45a4d2cba78480141bbf11019ea679e6c
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-04-13 19:12:00 -0500

    Update test_sound.py

--- resources/test_sound.py
+++ resources/test_sound.py
@@ -1,11 +1,19 @@
+#! /usr/bin/env python
+DEVICE = "RPI"
+DEVICE = "PC"
+
+
+
 from multiprocessing import Process
 
-from playsound import playsound
-
-def audio():
-    playsound("p0.mp3")
-
-
+if DEVICE == "RPI":
+    import os
+    def audio():
+        os.system('omxplayer sound.mp3')
+else:
+    from playsound import playsound
+    def audio():
+        playsound("p0.mp3")
 
 
 Process(target=audio).start()
