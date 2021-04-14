80ac93371f6f1e1f38bcc0d8895803eafacf1867
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-04-14 13:38:02 -0500

    Update config_movimiento_frente_camara.py

--- Process/Move_Person_Fron_Cam/config_movimiento_frente_camara.py
+++ Process/Move_Person_Fron_Cam/config_movimiento_frente_camara.py
@@ -2,8 +2,8 @@ TIEMPO_POR_INSTRUCCION = 15
 TIEMPO_AVISO_NO_MOVIMIENTO = 10
 TIEMPO_DESCANSO_ENTRE_UNA_PERSONA_Y_OTRA_CUANDO_LA_PRIMERA_COMPLETO_DESINFECCION = 35
 
-constante_cambio_area = 1000  # constante de cambio de area, entre mayor sea el numero, mayor es el area a detectar para cambio, es decir el equivalente a la velocidad de movimiento
-valor_cambio = 200  # Constante incremental para cambio de estado [Lavandose_manos, Persona_ausente_o_sin_movimiento]
+constante_cambio_area = 1200  # constante de cambio de area, entre mayor sea el numero, mayor es el area a detectar para cambio, es decir el equivalente a la velocidad de movimiento
+valor_cambio = 300 # 200  # Constante incremental para cambio de estado [Lavandose_manos, Persona_ausente_o_sin_movimiento]
 
 STATUS_MOVIMIENTO = ("MOVE", "NO_MOVE")
 
