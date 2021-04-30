ac00ae6fcd5cec09f1e5a9475231b18111311b6e
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-04-30 08:47:22 -0500

    Update CREATE_file_config_color.py

--- Process/Move_Person_Fron_Cam/CREATE_file_config_color.py
+++ Process/Move_Person_Fron_Cam/CREATE_file_config_color.py
@@ -1,3 +1,18 @@
+#sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev
+#sudo apt-get install libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
+#sudo apt-get install libgtk2.0-dev
+#sudo apt-get install libgtk-3-dev
+#sudo apt-get install libpng-dev
+# sudo apt-get install libjpeg-dev
+# sudo apt-get install libopenexr-dev
+# sudo apt-get install libtiff-dev
+# sudo apt-get install libwebp-dev
+
+# https://omes-va.com/como-instalar-opencv-en-raspberry-pi/
+# sudo apt-get install libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev libqtgui4 libqt4-test
+
+
+
 class Configuracion_filtro_camara(object):
     import cv2
     import numpy as np
