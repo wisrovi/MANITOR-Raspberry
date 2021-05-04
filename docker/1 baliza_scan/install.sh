c5dacfd5579fabd16bffe5f4c9402663768269db
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-05-04 07:11:10 -0500

    update

--- docker/1 baliza_scan/install.sh
+++ docker/1 baliza_scan/install.sh
@@ -2,4 +2,4 @@ sudo docker pull don41382/rpi-python3-with-bluetooth
 
 sudo docker build -t "beacon_scan:v1" .
 
-docker container run -d --net host --name=beacon_scan beacon_scan:v1
+docker container run -d --net host --name=beacon_scan  --restart=always beacon_scan:v1
--- docker/2 reproduce_audio/install.sh
+++ docker/2 reproduce_audio/install.sh
@@ -1,4 +1,4 @@
 
 docker build -t "reproduce__audio:v1" .
 
-docker container run -d --device /dev/snd --name=reproduce__audio  -p 5002:5002  reproduce__audio:v1
+docker container run -d --device /dev/snd --name=reproduce__audio --restart=always   -p 5002:5002  reproduce__audio:v1
