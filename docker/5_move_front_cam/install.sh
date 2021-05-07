1634a0ebff1afb7d1e9f888557935b9d114025f1
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-05-07 05:03:24 -0500

    update

--- docker/5_move_front_cam/install.sh
+++ docker/5_move_front_cam/install.sh
@@ -3,6 +3,6 @@ SUBSYSTEM=="vchiq",MODE="0666" >> /etc/udev/rules.d/99-camera.rules
 
 sudo docker build -t "move_front_cam:v1" .
 
-docker container run --privileged -d -v /opt/vc:/opt/vc -e LD_LIBRARY_PATH=/opt/vc/lib --name=move_front_cam -p 5006:5006 move_front_cam:v1
+docker container run --privileged -d --device=/dev/vchiq -v /opt/vc:/opt/vc -e LD_LIBRARY_PATH=/opt/vc/lib --name=move_front_cam -p 5006:5006 move_front_cam:v1
 #--device=/dev/vcsm
 #--device=/dev/vchiq
